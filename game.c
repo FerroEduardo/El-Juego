6291d1413ecc1073dbb128ffd79e84e522c512c4
FerroEduardo <eduardo.ferro.cabral@gmail.com>
2019-07-14 21:00:20 -0300

    REDUCED ALOCATED MEM, +ENEMIES[32]
    REDUCED ALOCATED MEM, +ENEMIES[32]

--- game.c
+++ game.c
@@ -12,7 +12,7 @@
 //VERSÃO
 #define version "v0.1.7" 
 /*
-    gcc -o game game.c -lSDL2 -lSDL2_image -lSDL2_mixer -Wall 
+    gcc -o game game.c -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf -Wall 
 -Wno-switch remove todos os warns relacionados ao Wswitch(n tem mais esse erro)
 compila e abre se nao tiver erro
     gcc -o game game.c -lSDL2 -lSDL2_image -lSDL2_mixer -Wall && ./game
@@ -55,7 +55,7 @@ double speedPlayer = 10;
 #define mapx 7680
 #define mapy 4320
 
-#define nEnemies 22
+#define nEnemies 32
 #define nBalao1 0
 #define nBalao2 1
 #define nFruits 3
@@ -109,7 +109,7 @@ int main(int argc, char* args[]){
     int statusGame=0,mageStatus=0;
     int sobe=false, desce=false, esquerda=false,direita=false,atacar=0,lastside,statusRank=0,statusCreditos=0;
     int enemyMove=-1,enemyMoveMinotaur=-1,minotaurAtack=false, falar_npc=false;
-    SDL_Surface *surfEnemies[nEnemies];
+    //SDL_Surface *surfEnemies[nEnemies];
     SDL_Texture *texEnemies[nEnemies];
     SDL_Rect rectEnemies[nEnemies];
     SDL_Rect rectspriteEnemies[nEnemies];
@@ -291,13 +291,13 @@ int main(int argc, char* args[]){
     //SDL_Rect rectloading = {0,0,SCREEN_WIDTH,SCREEN_HEIGHT};
     //---------------------
     //ENEMY1----------
-    //SDL_Surface *surfGreenEnemy = IMG_Load("recursos/green_enemy.png");
+    SDL_Surface *surfGreenEnemy = IMG_Load("recursos/green_enemy.png");
     //SDL_Texture *texGreenEnemy = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
-    //SDL_Surface *surfRedEnemy = IMG_Load("recursos/red_enemy.png");
+    SDL_Surface *surfRedEnemy = IMG_Load("recursos/red_enemy.png");
     //SDL_Texture *texRedEnemy = SDL_CreateTextureFromSurface(render, surfRedEnemy);
-    //SDL_Surface *surfTopEnemy = IMG_Load("recursos/top_enemy.png");
+    SDL_Surface *surfTopEnemy = IMG_Load("recursos/top_enemy.png");
     //SDL_Texture *texTopEnemy = SDL_CreateTextureFromSurface(render, surfTopEnemy);
-    //SDL_Surface *surfPurpleEnemy = IMG_Load("recursos/Purple_enemy.png");
+    SDL_Surface *surfPurpleEnemy = IMG_Load("recursos/purple_enemy.png");
     //SDL_Texture *texPurpleEnemy = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
     for(i=0;i<nEnemies;i++){
         rectspriteEnemies[i].x = 0;
@@ -308,73 +308,72 @@ int main(int argc, char* args[]){
         rectEnemies[i].h = 141;
     }
     //reeeeeeeeeeeeeeeeeeeeeeeeed---------------------------------------------------------
-    surfEnemies[0] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[0] = SDL_CreateTextureFromSurface(render, surfEnemies[0]);
+    texEnemies[0] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[0].x = 1060-rectBackground.x; rectEnemies[0].y = 1487-rectBackground.y;
-    surfEnemies[1] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[1] = SDL_CreateTextureFromSurface(render, surfEnemies[1]);
+    texEnemies[1] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[1].x = 1344-rectBackground.x; rectEnemies[1].y = 1410-rectBackground.y;
-    surfEnemies[2] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[2] = SDL_CreateTextureFromSurface(render, surfEnemies[2]);
+    texEnemies[2] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[2].x = 1277-rectBackground.x; rectEnemies[2].y = 1364-rectBackground.y;
-    surfEnemies[3] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[3] = SDL_CreateTextureFromSurface(render, surfEnemies[3]);
+    texEnemies[3] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[3].x = 1213-rectBackground.x; rectEnemies[3].y = 1763-rectBackground.y;
-    surfEnemies[4] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[4] = SDL_CreateTextureFromSurface(render, surfEnemies[4]);
+    texEnemies[4] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[4].x = 1207-rectBackground.x; rectEnemies[4].y = 1964-rectBackground.y;
-    surfEnemies[5] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[5] = SDL_CreateTextureFromSurface(render, surfEnemies[5]);
+    texEnemies[5] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[5].x = 1084-rectBackground.x; rectEnemies[5].y = 2129-rectBackground.y;
-    surfEnemies[6] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[6] = SDL_CreateTextureFromSurface(render, surfEnemies[6]);
+    texEnemies[6] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[6].x = 733-rectBackground.x; rectEnemies[6].y = 2165-rectBackground.y;
-    surfEnemies[7] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[7] = SDL_CreateTextureFromSurface(render, surfEnemies[7]);
+    texEnemies[7] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[7].x = 580-rectBackground.x; rectEnemies[7].y = 2087-rectBackground.y;
-    surfEnemies[8] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[8] = SDL_CreateTextureFromSurface(render, surfEnemies[8]);
+    texEnemies[8] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[8].x = 640-rectBackground.x; rectEnemies[8].y = 1892-rectBackground.y;
-    surfEnemies[9] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[9] = SDL_CreateTextureFromSurface(render, surfEnemies[9]);
+    texEnemies[9] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[9].x = 550-rectBackground.x; rectEnemies[9].y = 1775-rectBackground.y;
-    surfEnemies[10] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[10] = SDL_CreateTextureFromSurface(render, surfEnemies[10]);
+    texEnemies[10] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[10].x = 634-rectBackground.x; rectEnemies[10].y = 1601-rectBackground.y;
-    surfEnemies[11] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[11] = SDL_CreateTextureFromSurface(render, surfEnemies[11]);
+    texEnemies[11] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[11].x = 793-rectBackground.x; rectEnemies[11].y = 1490-rectBackground.y;
-    surfEnemies[12] = IMG_Load("recursos/red_enemy.png");
-    texEnemies[12] = SDL_CreateTextureFromSurface(render, surfEnemies[12]);
+    texEnemies[12] = SDL_CreateTextureFromSurface(render, surfRedEnemy);
     rectEnemies[12].x = 526-rectBackground.x; rectEnemies[12].y = 1421-rectBackground.y;
     //GREEEEEEEEEEEEEEEN-------------------------------------------------------------------
-    surfEnemies[13] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[13] = SDL_CreateTextureFromSurface(render, surfEnemies[13]);
+    texEnemies[13] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[13].x = 1029-rectBackground.x; rectEnemies[13].y = 2643-rectBackground.y;
-    surfEnemies[14] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[14] = SDL_CreateTextureFromSurface(render, surfEnemies[14]);
+    texEnemies[14] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[14].x = 2690-rectBackground.x; rectEnemies[14].y = 2751-rectBackground.y;
-    surfEnemies[15] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[15] = SDL_CreateTextureFromSurface(render, surfEnemies[15]);
+    texEnemies[15] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[15].x = 1500-rectBackground.x; rectEnemies[15].y = 2862-rectBackground.y;
-    surfEnemies[16] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[16] = SDL_CreateTextureFromSurface(render, surfEnemies[16]);
+    texEnemies[16] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[16].x = 777-rectBackground.x; rectEnemies[16].y = 2748-rectBackground.y;
-    surfEnemies[17] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[17] = SDL_CreateTextureFromSurface(render, surfEnemies[17]);
+    texEnemies[17] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[17].x = 549-rectBackground.x; rectEnemies[17].y = 2862-rectBackground.y;
-    surfEnemies[18] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[18] = SDL_CreateTextureFromSurface(render, surfEnemies[18]);
+    texEnemies[18] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[18].x = 621-rectBackground.x; rectEnemies[18].y = 3759-rectBackground.y;
-    surfEnemies[19] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[19] = SDL_CreateTextureFromSurface(render, surfEnemies[19]);
+    texEnemies[19] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[19].x = 651-rectBackground.x; rectEnemies[19].y = 3924-rectBackground.y;
-    surfEnemies[20] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[20] = SDL_CreateTextureFromSurface(render, surfEnemies[20]);
+    texEnemies[20] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[20].x = 846-rectBackground.x; rectEnemies[20].y = 4023-rectBackground.y;
-    surfEnemies[21] = IMG_Load("recursos/green_enemy.png");
-    texEnemies[21] = SDL_CreateTextureFromSurface(render, surfEnemies[21]);
+    texEnemies[21] = SDL_CreateTextureFromSurface(render, surfGreenEnemy);
     rectEnemies[21].x = 930-rectBackground.x; rectEnemies[21].y = 394-rectBackground.y;
+    //PURPLEEEEEEEEEEEEEEEEEEE-------------------------------------------------------------
+    texEnemies[22] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[22].x = 2544-rectBackground.x; rectEnemies[22].y = 3375-rectBackground.y;
+    texEnemies[23] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[23].x = 2889-rectBackground.x; rectEnemies[23].y = 3447-rectBackground.y;
+    texEnemies[24] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[24].x = 2907-rectBackground.x; rectEnemies[24].y = 3651-rectBackground.y;
+    texEnemies[25] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[25].x = 3258-rectBackground.x; rectEnemies[25].y = 3408-rectBackground.y;
+    texEnemies[26] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[26].x = 3258-rectBackground.x; rectEnemies[26].y = 3726-rectBackground.y;
+    texEnemies[27] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[27].x = 3591-rectBackground.x; rectEnemies[27].y = 3942-rectBackground.y;
+    texEnemies[28] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[28].x = 3786-rectBackground.x; rectEnemies[28].y = 3663-rectBackground.y;
+    texEnemies[29] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[29].x = 4194-rectBackground.x; rectEnemies[29].y = 3513-rectBackground.y;
+    texEnemies[30] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[30].x = 3876-rectBackground.x; rectEnemies[30].y = 3384-rectBackground.y;
+    texEnemies[31] = SDL_CreateTextureFromSurface(render, surfPurpleEnemy);
+    rectEnemies[31].x = 4227-rectBackground.x; rectEnemies[31].y = 3429-rectBackground.y;
     //-----------------
 
 
@@ -481,7 +480,7 @@ int main(int argc, char* args[]){
     rectWALL[67].x = 1464-rectBackground.x; rectWALL[67].y = 1512-rectBackground.y; rectWALL[67].w = 272; rectWALL[67].h = 862;
     rectWALL[67].x = 1383-rectBackground.x; rectWALL[67].y = 1512-rectBackground.y; rectWALL[67].w = 353; rectWALL[67].h = 148;
     rectWALL[68].x = 1485-rectBackground.x; rectWALL[68].y = 1601-rectBackground.y; rectWALL[68].w = 238; rectWALL[68].h = 536;
-    rectWALL[69].x = 1540-rectBackground.x; rectWALL[69].y = 3414-rectBackground.y; rectWALL[69].w = 202; rectWALL[69].h = 42;
+    rectWALL[69].x = 1540-rectBackground.x; rectWALL[69].y = 3414-rectBackground.y; rectWALL[69].w = 202; rectWALL[69].h = 42+6;
     rectWALL[70].x = 2184-rectBackground.x; rectWALL[70].y = 3476-rectBackground.y; rectWALL[70].w = 586; rectWALL[70].h = 855;
     rectWALL[71].x = 2055-rectBackground.x; rectWALL[71].y = 3523-rectBackground.y; rectWALL[71].w = 282; rectWALL[71].h = 148;
     rectWALL[72].x = 1926-rectBackground.x; rectWALL[72].y = 3583-rectBackground.y; rectWALL[72].w = 282; rectWALL[72].h = 148;
@@ -1292,7 +1291,7 @@ int main(int argc, char* args[]){
                 //--------------------------------------
 
                 
-                printf("x= %d , y= %d\n", rectBackground.x,rectBackground.y);
+                //printf("x= %d , y= %d\n", rectBackground.x,rectBackground.y);
 
 
 
@@ -1404,7 +1403,7 @@ int main(int argc, char* args[]){
         
     }
         for(i=0;i<nEnemies;i++){
-            SDL_FreeSurface(surfEnemies[i]);
+            //SDL_FreeSurface(surfEnemies[i]);
             SDL_DestroyTexture(texEnemies[i]);
         }
         for(i=0;i<nFruits;i++){
